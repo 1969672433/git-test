6a686f9abd4bfaee41e07e4509b338188612c13a
LQS <1969672433@qq.com>
2021-05-03 09:42:00 +0800

    master one

---
+++ src/main/java/com/qingsong/lqs.java
@@ -0,0 +1,11 @@
+package com.qingsong;
+
+/**
+ * @author LQS
+ * @create 2021--03-9:41
+ */
+public class lqs {
+    public static void main(String[] args) {
+        System.out.println("master one");
+    }
+}
